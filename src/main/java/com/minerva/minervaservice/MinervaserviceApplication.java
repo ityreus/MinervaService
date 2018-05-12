b65e1f8f79fac6b896e17739a3cd986816491ff0
Isaac Tyreus <isaac.tyreus@argos.co.uk>
2018-05-12 14:19:43 +0100

    Initial commit

---
+++ src/main/java/com/minerva/minervaservice/MinervaserviceApplication.java
@@ -0,0 +1,12 @@
+package com.minerva.minervaservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinervaserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinervaserviceApplication.class, args);
+	}
+}
